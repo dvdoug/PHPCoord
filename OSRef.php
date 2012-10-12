5ad2b20e4811adb2b4c9f5a2dd9679804ffa120e
Doug Wright <github@dougweb.org>
2012-10-12 16:54:36 +0100

    Minor cleanup

--- OSRef.php
+++ OSRef.php
@@ -9,16 +9,26 @@
 
   /**
    * Ordnance Survey grid reference
+   * References are accurate to 1m
    * @author Jonathan Stott
    * @author Doug Wright
    * @package PHPCoord
    */
-  // References given with OSRef are accurate to 1m.
   class OSRef {
 
-    var $easting;
-    var $northing;
+    /**
+     * Easting
+     * @api
+     * @var int
+     */
+    public $easting;
 
+    /**
+     * Northing
+     * @api
+     * @var int
+     */
+    public $northing;
 
     /**
      * Create a new OSRef object representing an OSGB grid reference. Note
@@ -33,57 +43,58 @@
      * a value representing an easting or northing accurate to 1mm would be
      * given as 651400.0001.
      *
-     * @param easting the easting of the reference (with 1m accuracy)
-     * @param northing the northing of the reference (with 1m accuracy)
+     * @param int $aEasting the easting of the reference (with 1m accuracy)
+     * @param int $aNorthing the northing of the reference (with 1m accuracy)
      */
-    function OSRef($easting, $northing) {
-      $this->easting  = $easting;
-      $this->northing = $northing;
+    public function __construct($aEasting, $aNorthing) {
+      $this->easting  = $aEasting;
+      $this->northing = $aNorthing;
     }
-
 
     /**
      * Convert this grid reference into a string showing the exact values
      * of the easting and northing.
-     *
-     * @return
+     * @return string
      */
     function toString() {
-      return "(" . $this->easting . ", " . $this->northing . ")";
+      return "({$this->easting}, {$this->northing})";
     }
-
 
     /**
      * Convert this grid reference into a string using a standard six-figure
      * grid reference including the two-character designation for the 100km
      * square. e.g. TG514131.
-     *
-     * @return
+     * @return string
      */
-    function toSixFigureString() {
+    public function toSixFigureString() {
       $hundredkmE = floor($this->easting / 100000);
       $hundredkmN = floor($this->northing / 100000);
       $firstLetter = "";
       if ($hundredkmN < 5) {
         if ($hundredkmE < 5) {
           $firstLetter = "S";
-        } else {
+        }
+        else {
           $firstLetter = "T";
         }
-      } else if ($hundredkmN < 10) {
+      }
+      else if ($hundredkmN < 10) {
         if ($hundredkmE < 5) {
           $firstLetter = "N";
         } else {
           $firstLetter = "O";
         }
-      } else {
+      }
+      else {
         $firstLetter = "H";
       }
 
       $secondLetter = "";
       $index = 65 + ((4 - ($hundredkmN % 5)) * 5) + ($hundredkmE % 5);
       $ti = $index;
-      if ($index >= 73) $index++;
+      if ($index >= 73) {
+        $index++;
+      }
       $secondLetter = chr($index);
 
       $e = round(($this->easting - (100000 * $hundredkmE)) / 100);
@@ -95,10 +106,9 @@
 
     /**
      * Convert this grid reference into a latitude and longitude
-     *
-     * @return
+     * @return LatLng
      */
-    function toLatLng() {
+    public function toLatLng() {
       $airy1830 = new RefEll(6377563.396, 6356256.909);
       $OSGB_F0  = 0.9996012717;
       $N0       = -100000.0;
@@ -131,43 +141,43 @@
                 * cos(3.0 * ($phiPrime + $phi0))));
         $phiPrime += ($N - $N0 - $M) / ($a * $OSGB_F0);
       } while (($N - $N0 - $M) >= 0.001);
-      $v = $a * $OSGB_F0 * pow(1.0 - $eSquared * sinSquared($phiPrime), -0.5);
+      $v = $a * $OSGB_F0 * pow(1.0 - $eSquared * pow(sin($phiPrime), 2), -0.5);
       $rho =
         $a
           * $OSGB_F0
           * (1.0 - $eSquared)
-          * pow(1.0 - $eSquared * sinSquared($phiPrime), -1.5);
+          * pow(1.0 - $eSquared * pow(sin($phiPrime), 2), -1.5);
       $etaSquared = ($v / $rho) - 1.0;
       $VII = tan($phiPrime) / (2 * $rho * $v);
       $VIII =
         (tan($phiPrime) / (24.0 * $rho * pow($v, 3.0)))
           * (5.0
-            + (3.0 * tanSquared($phiPrime))
+            + (3.0 * pow(tan($phiPrime), 2))
             + $etaSquared
-            - (9.0 * tanSquared($phiPrime) * $etaSquared));
+            - (9.0 * pow(tan($phiPrime), 2) * $etaSquared));
       $IX =
         (tan($phiPrime) / (720.0 * $rho * pow($v, 5.0)))
           * (61.0
-            + (90.0 * tanSquared($phiPrime))
-            + (45.0 * tanSquared($phiPrime) * tanSquared($phiPrime)));
-      $X = sec($phiPrime) / $v;
+            + (90.0 * pow(tan($phiPrime), 2))
+            + (45.0 * pow(tan($phiPrime), 2) * pow(tan($phiPrime), 2)));
+      $X = (1/cos($phiPrime)) / $v;
       $XI =
-        (sec($phiPrime) / (6.0 * $v * $v * $v))
-          * (($v / $rho) + (2 * tanSquared($phiPrime)));
+        ((1/cos($phiPrime)) / (6.0 * $v * $v * $v))
+          * (($v / $rho) + (2 * pow(tan($phiPrime), 2)));
       $XII =
-        (sec($phiPrime) / (120.0 * pow($v, 5.0)))
+        ((1/cos($phiPrime)) / (120.0 * pow($v, 5.0)))
           * (5.0
-            + (28.0 * tanSquared($phiPrime))
-            + (24.0 * tanSquared($phiPrime) * tanSquared($phiPrime)));
+            + (28.0 * pow(tan($phiPrime), 2))
+            + (24.0 * pow(tan($phiPrime), 2) * pow(tan($phiPrime), 2)));
       $XIIA =
-        (sec($phiPrime) / (5040.0 * pow($v, 7.0)))
+        ((1/cos($phiPrime)) / (5040.0 * pow($v, 7.0)))
           * (61.0
-            + (662.0 * tanSquared($phiPrime))
-            + (1320.0 * tanSquared($phiPrime) * tanSquared($phiPrime))
+            + (662.0 * pow(tan($phiPrime), 2))
+            + (1320.0 * pow(tan($phiPrime), 2) * pow(tan($phiPrime), 2))
             + (720.0
-              * tanSquared($phiPrime)
-              * tanSquared($phiPrime)
-              * tanSquared($phiPrime)));
+              * pow(tan($phiPrime), 2)
+              * pow(tan($phiPrime), 2)
+              * pow(tan($phiPrime), 2)));
       $phi =
         $phiPrime
           - ($VII * pow($E - $E0, 2.0))
@@ -182,89 +192,41 @@
 
       return new LatLng(rad2deg($phi), rad2deg($lambda));
     }
-  }
 
-  // ================================================== Mathematical Functions
-
-  function sinSquared($x) {
-    return sin($x) * sin($x);
-  }
-
-  function cosSquared($x) {
-    return cos($x) * cos($x);
-  }
-
-  function tanSquared($x) {
-    return tan($x) * tan($x);
-  }
-
-  function sec($x) {
-    return 1.0 / cos($x);
-  }
-
-
-  /**
-   * Take a string formatted as a six-figure OS grid reference (e.g.
-   * "TG514131") and return a reference to an OSRef object that represents
-   * that grid reference. The first character must be H, N, S, O or T.
-   * The second character can be any uppercase character from A through Z
-   * excluding I.
-   *
-   * @param ref
-   * @return
-   * @since 2.1
-   */
-  function getOSRefFromSixFigureReference($ref) {
-    $char1 = substr($ref, 0, 1);
-    $char2 = substr($ref, 1, 1);
-    $east  = substr($ref, 2, 3) * 100;
-    $north = substr($ref, 5, 3) * 100;
-    if ($char1 == 'H') {
-      $north += 1000000;
-    } else if ($char1 == 'N') {
-      $north += 500000;
-    } else if ($char1 == 'O') {
-      $north += 500000;
-      $east  += 500000;
-    } else if ($char1 == 'T') {
-      $east += 500000;
+    /**
+     * Take a string formatted as a six-figure OS grid reference (e.g.
+     * "TG514131") and return a reference to an OSRef object that represents
+     * that grid reference. The first character must be H, N, S, O or T.
+     * The second character can be any uppercase character from A through Z
+     * excluding I.
+     *
+     * @param string $aRef
+     * @return OSRef
+     */
+    public static function getOSRefFromSixFigureReference($aRef) {
+      $char1 = substr($aRef, 0, 1);
+      $char2 = substr($aRef, 1, 1);
+      $east  = substr($aRef, 2, 3) * 100;
+      $north = substr($aRef, 5, 3) * 100;
+      if ($char1 == 'H') {
+        $north += 1000000;
+      }
+      else if ($char1 == 'N') {
+        $north += 500000;
+      }
+      else if ($char1 == 'O') {
+        $north += 500000;
+        $east  += 500000;
+      }
+      else if ($char1 == 'T') {
+        $east += 500000;
+      }
+      $char2ord = ord($char2);
+      if ($char2ord > 73) { // Adjust for no I
+        $char2ord--; 
+      }
+      $nx = (($char2ord - 65) % 5) * 100000;
+      $ny = (4 - floor(($char2ord - 65) / 5)) * 100000;
+      return new OSRef($east + $nx, $north + $ny);
     }
-    $char2ord = ord($char2);
-    if ($char2ord > 73) $char2ord--; // Adjust for no I
-    $nx = (($char2ord - 65) % 5) * 100000;
-    $ny = (4 - floor(($char2ord - 65) / 5)) * 100000;
-    return new OSRef($east + $nx, $north + $ny);
   }
-
-
-  /**
-   *  Work out the UTM latitude zone from the latitude
-   *
-   * @param latitude
-   * @return
-   */
-  function getUTMLatitudeZoneLetter($latitude) {
-    if ((84 >= $latitude) && ($latitude >= 72)) return "X";
-    else if (( 72 > $latitude) && ($latitude >=  64)) return "W";
-    else if (( 64 > $latitude) && ($latitude >=  56)) return "V";
-    else if (( 56 > $latitude) && ($latitude >=  48)) return "U";
-    else if (( 48 > $latitude) && ($latitude >=  40)) return "T";
-    else if (( 40 > $latitude) && ($latitude >=  32)) return "S";
-    else if (( 32 > $latitude) && ($latitude >=  24)) return "R";
-    else if (( 24 > $latitude) && ($latitude >=  16)) return "Q";
-    else if (( 16 > $latitude) && ($latitude >=   8)) return "P";
-    else if ((  8 > $latitude) && ($latitude >=   0)) return "N";
-    else if ((  0 > $latitude) && ($latitude >=  -8)) return "M";
-    else if (( -8 > $latitude) && ($latitude >= -16)) return "L";
-    else if ((-16 > $latitude) && ($latitude >= -24)) return "K";
-    else if ((-24 > $latitude) && ($latitude >= -32)) return "J";
-    else if ((-32 > $latitude) && ($latitude >= -40)) return "H";
-    else if ((-40 > $latitude) && ($latitude >= -48)) return "G";
-    else if ((-48 > $latitude) && ($latitude >= -56)) return "F";
-    else if ((-56 > $latitude) && ($latitude >= -64)) return "E";
-    else if ((-64 > $latitude) && ($latitude >= -72)) return "D";
-    else if ((-72 > $latitude) && ($latitude >= -80)) return "C";
-    else return 'Z';
-  }
-
-?>
