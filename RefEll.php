7f9804428eeba6373b21132781cf6a99c689e18a
Doug Wright <github@dougweb.org>
2012-10-13 17:30:57 +0100

    Add some more well-known ellipsoids

--- RefEll.php
+++ RefEll.php
@@ -61,6 +61,47 @@
      * @return RefEll
      */
     public static function WGS84() {
-      return new RefEll(6378137, 6356752.314);
+      return new RefEll(6378137, 6356752.314245);
     }
+    
+    /**
+     * Helper function to create GRS80 ellipsoid
+     * @return RefEll
+     */
+    public static function GRS80() {
+      return new RefEll(6378137, 6356752.314140);
+    }
+    
+    /**
+     * Helper function to create GRS80 ellipsoid
+     * @return RefEll
+     */
+    public static function Clarke1866() {
+      return new RefEll(6378206.4, 6356583.8);
+    }
+    
+    /**
+     * Helper function to create International 1924 (Heyford) ellipsoid
+     * @return RefEll
+     */
+    public static function International1924() {
+      return new RefEll(6378388.0, 6356911.9);
+    }
+    
+    /**
+     * Helper function to create International 1924 (Heyford) ellipsoid
+     * @return RefEll
+     */
+    public static function Heyford1924() {
+      return new RefEll(6378388.0, 6356911.9);
+    }
+    
+    /**
+     * Helper function to create Bessel 1841 ellipsoid
+     * @return RefEll
+     */
+    public static function Bessel1841() {
+      return new RefEll(6377397.155, 6356078.963);
+    }
+    
   }
