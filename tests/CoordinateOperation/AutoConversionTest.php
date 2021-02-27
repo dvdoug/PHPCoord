dd9133d026f94130219924f7eae2cb2bcb609e6e
Doug Wright <github@dougweb.org>
2021-02-27 22:07:01 +0000

    Test 3D and 2D conversions from Compound points

--- tests/CoordinateOperation/AutoConversionTest.php
+++ tests/CoordinateOperation/AutoConversionTest.php
@@ -129,7 +129,18 @@ class AutoConversionTest extends TestCase
         self::assertEqualsWithDelta(5827721.404, $to->getNorthing()->getValue(), 0.001);
     }
 
-    public function testAutoConversionTokyoJSLDToWGS84(): void
+    public function testAutoConversionTokyoJSLDToWGS842D(): void
+    {
+        $from = CompoundPoint::create(GeographicPoint::create(new Degree(35.689722), new Degree(139.692222), null, Geographic2D::fromSRID(Geographic2D::EPSG_TOKYO)), VerticalPoint::create(new Metre(100), Vertical::fromSRID(Vertical::EPSG_JSLD69_HEIGHT)), Compound::fromSRID(Compound::EPSG_TOKYO_PLUS_JSLD69_HEIGHT));
+        $toCRS = Geographic2D::fromSRID(Geographic2D::EPSG_WGS_84);
+        $to = $from->convert($toCRS);
+
+        self::assertEqualsWithDelta(35.692958, $to->getLatitude()->getValue(), 0.000001);
+        self::assertEqualsWithDelta(139.689019, $to->getLongitude()->getValue(), 0.000001);
+        self::assertNull($to->getHeight());
+    }
+
+    public function testAutoConversionTokyoJSLDToWGS843D(): void
     {
         $from = CompoundPoint::create(GeographicPoint::create(new Degree(35.689722), new Degree(139.692222), null, Geographic2D::fromSRID(Geographic2D::EPSG_TOKYO)), VerticalPoint::create(new Metre(100), Vertical::fromSRID(Vertical::EPSG_JSLD69_HEIGHT)), Compound::fromSRID(Compound::EPSG_TOKYO_PLUS_JSLD69_HEIGHT));
         $toCRS = Geographic3D::fromSRID(Geographic3D::EPSG_WGS_84);
