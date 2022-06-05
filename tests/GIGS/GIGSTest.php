90849f6a785aacb2d12c845cfbbb246afed7bd10
Doug Wright <github@dougweb.org>
2022-06-05 16:36:10 +0100

    Add GIGS series 5110 tests

--- tests/GIGS/GIGSTest.php
+++ tests/GIGS/GIGSTest.php
@@ -900,6 +900,7 @@ class GIGSTest extends TestCase
      * @dataProvider series5100AmericanPolyconicData
      * @dataProvider series5100CassiniSoldnerData
      * @dataProvider series5100AlbersData
+     * @dataProvider series5100LAEAData
      */
     public function testSeries5100Projections(Angle $latitude, Angle $longitude, Length $easting, Length $northing, bool $inReverse, string $geographicCRSSrid, string $projectedCRSSrid, float $cartesianTolerance, float $geographicTolerance, float $roundTripCartesianTolerance, float $roundTripGeographicTolerance, bool $roundTripPoint): void
     {
@@ -1209,6 +1210,23 @@ class GIGSTest extends TestCase
             };
 
             yield '#' . $row[0] => [new Degree((float) $row[1]), new Degree((float) $row[2]), new Metre((float) $row[3]), new Metre((float) $row[4]), $direction, $geographicCRSSrid, $projectedCRSSrid, (float) $header['Cartesian Tolerance'], (float) $header['Geographic Tolerance'], (float) $header['Round Trip Cartesian Tolerance'], (float) $header['Round Trip Geographic Tolerance'], isset($row[7]) ? $row[7] === 'Round Trip calculation point' : false];
+        }
+    }
+
+    public function series5100LAEAData(): Generator
+    {
+        [$header, $body] = $this->parseDataFile(__DIR__ . '/GIGS 5100 Conversion test data/ASCII/GIGS_conv_5110_LAEA_output.txt');
+
+        $geographicCRSSrid = 'urn:ogc:def:crs:GIGS::64010';
+        $projectedCRSSrid = 'urn:ogc:def:crs:GIGS::62023';
+
+        foreach ($body as $row) {
+            $direction = match ($row['6']) {
+                'FORWARD' => false,
+                'REVERSE' => true,
+            };
+
+            yield '#' . $row[0] => [new Degree((float) $row[1]), new Degree((float) $row[2]), new Metre((float) $row[4]), new Metre((float) $row[3]), $direction, $geographicCRSSrid, $projectedCRSSrid, (float) $header['Cartesian Tolerance'], (float) $header['Geographic Tolerance'], (float) $header['Round Trip Cartesian Tolerance'], (float) $header['Round Trip Geographic Tolerance'], isset($row[7]) ? $row[7] === 'Round Trip calculation point' : false];
         }
     }
 
