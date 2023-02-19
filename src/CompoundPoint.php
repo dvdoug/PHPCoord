503b4e3e72b2018c38f00c6bcbebba3e26a4738d
Doug Wright <github@dougweb.org>
2023-02-19 21:24:43 +0000

    #50 Better variable naming

--- src/CompoundPoint.php
+++ src/CompoundPoint.php
@@ -149,23 +149,20 @@ class CompoundPoint extends Point implements ConvertiblePoint
             if ($to instanceof Geographic3D) {
                 // try converting to any/all of the other Compound CRSs that include the same vertical CRS, where the
                 // horizontal CRS has a 3D equivalent. From there, try converting using the usual mechanisms
-
                 $candidateIntermediates = Compound::findFromVertical($this->getVerticalPoint()->getCRS());
                 unset($candidateIntermediates[$this->getCRS()->getSRID()]);
 
                 foreach ($candidateIntermediates as $candidateIntermediate) {
                     try {
                         if ($candidateIntermediate->getHorizontal() instanceof Geographic2D && $candidateIntermediate->getHorizontal()->getBaseCRS() instanceof Geographic3D) {
-                            $newHorizontalPoint = $this->getHorizontalPoint()->convert($candidateIntermediate->getHorizontal());
-                            $workingPoint = self::create(
+                            $candidateHorizontalPoint = $this->getHorizontalPoint()->convert($candidateIntermediate->getHorizontal());
+                            $candidateIntermediatePoint = self::create(
                                 $candidateIntermediate,
-                                $newHorizontalPoint,
+                                $candidateHorizontalPoint,
                                 $this->getVerticalPoint()
                             );
 
-                            $candidateIntermediatePoint = $workingPoint->convert($candidateIntermediate->getHorizontal()->getBaseCRS());
-
-                            return $candidateIntermediatePoint->convert($to);
+                            return $candidateIntermediatePoint->convert($candidateIntermediate->getHorizontal()->getBaseCRS())->convert($to);
                         }
                     } catch (Throwable) {
                     }
